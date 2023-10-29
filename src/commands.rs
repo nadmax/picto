51ee3d3e397806e55f5f6167b76ff9c7767e5e07
nadmax <n.maximilien1@gmail.com>
2023-10-29 14:46:23 +0100

    remove as much as possible unwrap usage

--- src/commands.rs
+++ src/commands.rs
@@ -1,15 +1,15 @@
-use std::fs::write;
-use std::str::FromStr;
-
 use crate::chunk::Chunk;
 use crate::chunk_type::ChunkType;
 use crate::picto::{DecodeArgs, EncodeArgs, ListArgs, RemoveArgs};
 use crate::png::Png;
 use crate::xor;
-use crate::Result;
+use crate::{Error, Result};
+
+use std::fs::write;
+use std::str::FromStr;
 
 pub fn encode(args: EncodeArgs) -> Result<()> {
-    let chunk_type = ChunkType::from_str(&args.chunk_type).unwrap();
+    let chunk_type = ChunkType::from_str(&args.chunk_type)?;
     let encoded_message = xor::xor_encode(args.message.as_bytes(), &args.key);
     let chunk = Chunk::new(chunk_type, encoded_message);
     let mut png = Png::from_file(&args.path)?;
@@ -31,10 +31,10 @@ pub fn decode(args: DecodeArgs) -> Result<()> {
     let chunk = png.chunk_by_type(&args.chunk_type);
 
     match chunk {
-        Some(c) => {
-            let decoded_message = xor::xor_decode(c.data(), &args.key);
-            println!("Decoded message: {decoded_message}");
-        }
+        Some(c) => match xor::xor_decode(c.data(), &args.key) {
+            Ok(decoded_message) => println!("Decoded message: {decoded_message}"),
+            Err(err) => return Err(Error::new(err)),
+        },
         None => println!("No message for Chunk type '{}'", args.chunk_type),
     }
 
